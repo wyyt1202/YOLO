53099e28b83ba48ef3e3893032265a5ceca5f8e1
wyyt1202 <1204550310@qq.com>
2025-02-01 15:48:16 +0800

    MCF-Net

--- models/extral_block.py
+++ models/extral_block.py
@@ -80,87 +80,16 @@ class ADown(nn.Module):
         x2 = self.cv2(x2)
         return torch.cat((x1, x2), 1)
 class MCAM(nn.Module):
-    def __init__(self, inc, kernel_sizes=(5, 7, 9, 11), e=0.5) -> None:
-        super().__init__()
-        hidc = int(inc[1] * e)
-        
-        self.conv1 = nn.Sequential(
-            nn.Upsample(scale_factor=2),
-            Conv(inc[0], hidc, 1)
-        )
-        self.conv2 = Conv(inc[1], hidc, 1) if e != 1 else nn.Identity()
-        self.conv3 = ADown(inc[2], hidc)
-               
-        self.dw_conv = nn.ModuleList(nn.Conv2d(hidc * 3, hidc * 3, kernel_size=k, padding=autopad(k), groups=hidc * 3) for k in kernel_sizes)
-        self.pw_conv = Conv(hidc * 3, hidc * 3)
-    
-    def forward(self, x):
-        x1, x2, x3 = x
-        x1 = self.conv1(x1)
-        x2 = self.conv2(x2)
-        x3 = self.conv3(x3)
-        
-        x = torch.cat([x1, x2, x3], dim=1)
-        feature = torch.sum(torch.stack([x] + [layer(x) for layer in self.dw_conv], dim=0), dim=0)
-        feature = self.pw_conv(feature)
-        
-        x = x + feature
-        return x
+    # 论文发表后将会开源
+    return x
 ################################################################################
-################################################################################
-def drop_path(x, drop_prob: float = 0., training: bool = False):
-    """Drop paths (Stochastic Depth) per sample (when applied in main path of residual blocks).
-    This is the same as the DropConnect impl I created for EfficientNet, etc networks, however,
-    the original name is misleading as 'Drop Connect' is a different form of dropout in a separate paper...
-    See discussion: https://github.com/tensorflow/tpu/issues/494#issuecomment-532968956 ... I've opted for
-    changing the layer and argument names to 'drop path' rather than mix DropConnect as a layer name and use
-    'survival rate' as the argument.
-    """
-    if drop_prob == 0. or not training:
-        return x
-    keep_prob = 1 - drop_prob
-    shape = (x.shape[0],) + (1,) * (x.ndim - 1)  # work with diff dim tensors, not just 2D ConvNets
-    random_tensor = keep_prob + torch.rand(shape, dtype=x.dtype, device=x.device)
-    random_tensor.floor_()  # binarize
-    output = x.div(keep_prob) * random_tensor
-    return output
-class DropPath(nn.Module):
-    """Drop paths (Stochastic Depth) per sample  (when applied in main path of residual blocks).
-    """
-    
-    def __init__(self, drop_prob=None):
-        super(DropPath, self).__init__()
-        self.drop_prob = drop_prob
-    
-    def forward(self, x):
-        return drop_path(x, self.drop_prob, self.training)
-    
 class LS_Block(nn.Module):
-    def __init__(self, dim, mlp_ratio=3, drop_path=0.):
-        super().__init__()
-        self.dwconv = Conv(dim, dim, 7, g=dim, act=False)
-        self.f1 = nn.Conv2d(dim, mlp_ratio * dim, 1)
-        self.f2 = nn.Conv2d(dim, mlp_ratio * dim, 1)
-        self.g = Conv(mlp_ratio * dim, dim, 1, act=False)
-        self.dwconv2 = nn.Conv2d(dim, dim, 7, 1, (7 - 1) // 2, groups=dim)
-        self.act = nn.ReLU6()
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-
-    def forward(self, x):
-        input = x
-        x = self.dwconv(x)
-        x1, x2 = self.f1(x), self.f2(x)
-        x = self.act(x1) * x2
-        x = self.dwconv2(self.g(x))
-        x = input + self.drop_path(x)
-        return x
+    # 论文发表后将会开源
+    return x
 
 class C3_LS(C3):
     def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
         super().__init__(c1, c2, n, shortcut, g, e)
         c_ = int(c2 * e)  # hidden channels
         self.m = nn.Sequential(*(LS_Block(c_) for _ in range(n)))
-
-
-
 ######################################## StartNet end ########################################
